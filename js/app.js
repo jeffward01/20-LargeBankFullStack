f058aedbc0512cd6ef54b823ad8373c690ab2ec7
jeffward01 <jeffwardcoding@gmail.com>
2015-10-22 19:04:39 -0700

    added close button

--- js/app.js
+++ js/app.js
@@ -49,6 +49,24 @@
         self.AllCustomers = new CustomerModel();
         self.selectedCustomerAccounts = ko.observableArray();
         self.saveCustomer = function () {
+            //validation
+            validateColor($("#InputFirstName"));
+            validateColor($("#InputLastName"));
+            validateColor($("#InputAddress1"));
+            validateColor($("#InputCity"));
+            validateColor($("#InputZip"));
+            validateColor($("#InputState"));
+            validateState($("#InputState"));
+           
+            var inputArray = [$("#InputFirstName"), $("#InputLastName"), $("#InputAddress1"), $("#InputAddress1"), $("#InputCity"), $("#InputZip") ];
+            
+           if(!validate(inputArray)){
+               return;
+           }
+
+
+
+
             if (self.page() == 'customer.add') {
                 $.ajax({
                     type: 'POST',
@@ -101,11 +119,6 @@
                 return;
             }
 
-
-
-
-
-
             if (self.page() == 'account.add') {
                 $.ajax({
                     type: 'POST',
@@ -124,138 +137,169 @@
             }
             alert("New Account Added! (Need Permission from API 'Error')");
         }
-    
 
 
-    //Customer Grid
-    self.customers = ko.observableArray();
 
-    //Naviage to add new Customer page
-    self.addCustomer = function () {
-        self.page('customer.add');
+        //Customer Grid
+        self.customers = ko.observableArray();
 
-    };
+        //Naviage to add new Customer page
+        self.addCustomer = function () {
+            self.page('customer.add');
 
-    //Navigate to add new Account Page
-    self.addAccount = function () {
-          //Clear Previous Red lines
+        };
+
+        //Navigate to add new Account Page
+        self.addAccount = function () {
+            //Clear Previous Red lines
             $('#InputAccountBalance').removeClass("red-border");
-             $('#InputAccountNumber').removeClass("red-border");
-        
-        
-        
-        self.page('account.add')
-    }
+            $('#InputAccountNumber').removeClass("red-border");
 
-    //Edit a Customer
 
-    self.editCustomer = function (customer) {
-        $.ajax({
-            type: 'GET',
-            url: 'http://localhost:49690/api/customers/' + customer.CustomerId(),
-            success: function (data) {
-                ko.mapping.fromJS(data, {}, self.selectedCustomer);
 
+            self.page('account.add')
+        }
+
+        //Edit a Customer
+
+        self.editCustomer = function (customer) {
+            $.ajax({
+                type: 'GET',
+                url: 'http://localhost:49690/api/customers/' + customer.CustomerId(),
+                success: function (data) {
+                    ko.mapping.fromJS(data, {}, self.selectedCustomer);
+
+                    $.ajax({
+                        type: 'GET',
+                        url: 'http://localhost:49690/api/customers/' + customer.CustomerId() + '/accounts',
+                        success: function (data) {
+                            ko.mapping.fromJS(data, {}, self.selectedCustomerAccounts);
+
+                            self.page('customer.detail');
+                        }
+                    });
+                }
+            });
+        };
+
+        self.viewCustomerAccounts = function (customer) {
                 $.ajax({
                     type: 'GET',
                     url: 'http://localhost:49690/api/customers/' + customer.CustomerId() + '/accounts',
                     success: function (data) {
-                        ko.mapping.fromJS(data, {}, self.selectedCustomerAccounts);
+                        ko.mapping.fromJS(ko.mapping.toJS(customer), {}, self.selectedCustomer);
 
-                        self.page('customer.detail');
+
+                        ko.mapping.fromJS(data, {}, self.customersAccounts);
+
+
+                        self.page('customer.accounts');
                     }
-                });
-            }
-        });
-    };
 
-    self.viewCustomerAccounts = function (customer) {
+
+                })
+
+            }
+            //Displays all of current customer's account in Modal
+        self.displayCustomerAccountsModal = function () {
             $.ajax({
                 type: 'GET',
-                url: 'http://localhost:49690/api/customers/' + customer.CustomerId() + '/accounts',
+                url: 'http://localhost:49690/api/customers/' + self.selectedCustomer.CustomerId() + '/accounts',
                 success: function (data) {
-                    ko.mapping.fromJS(ko.mapping.toJS(customer), {}, self.selectedCustomer);
-
 
                     ko.mapping.fromJS(data, {}, self.customersAccounts);
 
-
-                    self.page('customer.accounts');
                 }
 
 
             })
 
+
         }
-        //Displays all of current customer's account in Modal
-    self.displayCustomerAccountsModal = function () {
-        $.ajax({
-            type: 'GET',
-            url: 'http://localhost:49690/api/customers/' + self.selectedCustomer.CustomerId() + '/accounts',
-            success: function (data) {
 
-                ko.mapping.fromJS(data, {}, self.customersAccounts);
+        //Delete A customer
+        self.deleteCustomer = function (customer) {
+            if (confirm("Are you sure you wish to delete this customer? This cannot be undone.")) {
+                $.ajax({
+                    type: 'DELETE',
+                    url: 'http://localhost:49690/api/customers?id=' + customer.CustomerId(),
+                    success: function (data) {
+                        alert("Customer has been deleted");
 
+                        self.reloadCustomers();
+                    }
+                });
             }
+        };
 
+        /*
+        self.reload = function() {
+            
+            customers = function(){
+                $.ajax({
+                    type: 'GET',
+                    url: 'http://localhost:49690/api/customers/',
+                    success: function (data) {
+                        ko.mapping.fromJS(data, {}, self.customers);
+         
+                        self.page('customer.grid');
+                    }
+                });     
+            }
+            
+        };
+          */
 
-        })
-
-
-    }
-
-    //Delete A customer
-    self.deleteCustomer = function (customer) {
-        if (confirm("Are you sure you wish to delete this customer? This cannot be undone.")) {
-            $.ajax({
-                type: 'DELETE',
-                url: 'http://localhost:49690/api/customers?id=' + customer.CustomerId(),
-                success: function (data) {
-                    alert("Customer has been deleted");
-
-                    self.reloadCustomers();
-                }
-            });
-        }
-    };
-
-    /*
-    self.reload = function() {
-        
-        customers = function(){
+        self.reloadCustomers = function () {
+            //Populate Customer Grid with ALL Customers
             $.ajax({
                 type: 'GET',
                 url: 'http://localhost:49690/api/customers/',
                 success: function (data) {
                     ko.mapping.fromJS(data, {}, self.customers);
-     
+
+
                     self.page('customer.grid');
                 }
-            });     
+            });
+
         }
-        
+        self.CustomerGrid = function(){
+            self.page('customer.grid')
+        }
+
+        self.reloadCustomers();
+
+
     };
-      */
 
-    self.reloadCustomers = function () {
-        //Populate Customer Grid with ALL Customers
-        $.ajax({
-            type: 'GET',
-            url: 'http://localhost:49690/api/customers/',
-            success: function (data) {
-                ko.mapping.fromJS(data, {}, self.customers);
-
-
-                self.page('customer.grid');
-            }
-        });
-
+    function validateColor(x) {
+        if ($(x).val() == "") {
+            $(x).addClass("red-border");
+        }
     }
 
-    self.reloadCustomers();
+    function validate(arr) {
+        for (var i = 0; i < arr.length; i++) {
+            if ($(arr[i].val() == "")) {
+                alert("Please fill in all of the required Inputs");
+                return false;
+            }
+        }
+    }
+
+    function validateState(y) {
+        x = $(y).val();
+        x = x.replace(/[^a-z\d\s]+/gi, "");
+        x = x.replace(/[0-9]/g, '');
+        if (x.length > 2) {
+            alert("Please enter the State in the correct Format!");
+            return;
+        }
 
 
-    };
+
+    }
 
 
     ko.applyBindings(new MyViewModel());
